f09421d68c9f8b5adb0f615c058f367c4646a0a2
Martijn Cuppens <martijn.cuppens@gmail.com>
2023-10-05 15:29:42 +0200

    Get rid of the universal selector

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import plugin from 'tailwindcss/plugin'
-import config from 'tailwindcss/stubs/config.full'
+import plugin from 'tailwindcss/plugin';
+import config from 'tailwindcss/stubs/config.full';
+import isPlainObject from 'tailwindcss/lib/util/isPlainObject';
 
 export default plugin.withOptions((
   {
@@ -14,97 +15,117 @@ export default plugin.withOptions((
     {
       matchUtilities,
       theme,
-      addDefaults,
       addBase,
       addVariant,
       e,
     },
   ) => {
-    // Add line-height to every element
-    addDefaults('line-height', {
-      'line-height': 'var(--tw-lh)',
-    })
-
-    // We need to add the line height to several form control selectors too
-    // because their line-height is set in the css reset.
-    addBase(Object.fromEntries(['button', 'input', 'optgroup', 'select', 'textarea'].map((selector) => {
-      return [selector, {
-        lineHeight: 'var(--tw-lh)',
-      }]
-    })))
-
     // Define the base line-height in the root.
     addBase({
       ':root': {
-        '--tw-lh': theme('lineHeight').normal,
+        '--tw-lh': theme('lineHeight').normal ?? 'min(1.5em, 1em + .5rem)', // Fail-safe when normal is line height is removed from config
       },
-    })
+      // Add line height for elements that have a font size set by default.
+      'small,rt': {
+        'line-height': 'var(--tw-lh)',
+      },
+    });
 
-    // Use custom properties instead of line-heights
+    // We need to override the excisting font size utility, to make the line heights work.
+    matchUtilities(
+      {
+        text: (value, {modifier}) => {
+          let [fontSize, options] = Array.isArray(value) ? value : [value];
+
+          if (modifier) {
+            return {
+              'font-size': fontSize,
+              'line-height': modifier,
+            };
+          }
+
+          let {lineHeight, letterSpacing, fontWeight} = isPlainObject(options)
+            ? options
+            : {lineHeight: options};
+
+          return {
+            'font-size': fontSize,
+            // We always need to set the line height, either with the line height
+            // that is defined, or we recalculate the line height with the custom property.
+            'line-height': lineHeight === undefined ? 'var(--tw-lh)' : lineHeight,
+            ...(letterSpacing === undefined ? {} : {'letter-spacing': letterSpacing}),
+            ...(fontWeight === undefined ? {} : {'font-weight': fontWeight}),
+          };
+        },
+      },
+      {
+        values: theme('fontSize'),
+        modifiers: theme('lineHeight'),
+        type: ['absolute-size', 'relative-size', 'length', 'percentage'],
+      },
+    );
+
+    // Use custom properties instead of line-heights.
     matchUtilities(
       {
         leading: (value) => ({
           '--tw-lh': value,
         }),
-      },
-      {values: theme('lineHeight')},
-    )
+      }, {values: theme('lineHeight')},
+    );
 
     addVariant('fluid', ({modifySelectors, separator, container}) => {
       container.walkDecls(decl => {
-
-        // Turn line height modifiers in custom properties.
-        if (decl.prop === 'line-height') {
-          decl.prop = '--tw-lh';
-        } else {
+        // Exclude line-height modifiers.
+        if (decl.prop !== 'line-height') {
           decl.value = decl.value.replace(/(-?\d*\.?\d+)(px|rem)/g, (match, stringValue, unit) => {
-            let value = Number.parseFloat(stringValue)
+            let value = Number.parseFloat(stringValue);
 
             // Convert to px if in rem
             if (unit === 'rem') {
-              value *= remValue
+              value *= remValue;
             }
 
             // Only add responsive function if needed
             if (minimum >= Math.abs(value) || factor <= 1) {
-              return `${stringValue}${unit}`
+              return `${stringValue}${unit}`;
             }
 
             // Calculate base and difference
-            let baseValue = minimum + ((Math.abs(value) - minimum) / factor)
-            const diff = Math.abs(value) - baseValue
+            let baseValue = minimum + ((Math.abs(value) - minimum) / factor);
+            const diff = Math.abs(value) - baseValue;
 
             // Divide by remValue if needed
             if (unit === 'rem') {
-              baseValue /= remValue
+              baseValue /= remValue;
             }
 
-            const viewportUnit = twoDimensional ? 'vmin' : 'vw'
-            const sign = value > 0 ? '+' : '-'
-            const func = value > 0 ? 'min' : 'max'
+            const viewportUnit = twoDimensional ? 'vmin' : 'vw';
+            const sign = value > 0 ? '+' : '-';
+            const func = value > 0 ? 'min' : 'max';
 
-            baseValue = value > 0 ? baseValue : -baseValue
-            value = `${Number((unit === 'rem' ? value / remValue : value).toFixed(unitPrecision))}${unit}`
-            const fluidValue = diff * 100 / breakpoint
+            baseValue = value > 0 ? baseValue : -baseValue;
+            value = `${Number((unit === 'rem' ? value / remValue : value).toFixed(unitPrecision))}${unit}`;
+            const fluidValue = diff * 100 / breakpoint;
 
-            return `${func}(${value}, ${Number(baseValue.toFixed(unitPrecision))}${unit} ${sign} ${Number(fluidValue.toFixed(unitPrecision))}${viewportUnit})`
-          })
+            return `${func}(${value}, ${Number(baseValue.toFixed(unitPrecision))}${unit} ${sign} ${Number(fluidValue.toFixed(unitPrecision))}${viewportUnit})`;
+          });
         }
-      })
+      });
 
       modifySelectors(({className}) => {
-        return `.${e(`fluid${separator}${className}`)}`
-      })
-    })
-  }
+        return `.${e(`fluid${separator}${className}`)}`;
+      });
+    });
+  };
 }, () => {
 
   return {
     theme: {
       // Remove line height from font sizes
       fontSize: Object.fromEntries(Object.entries(config.theme.fontSize).map(([key, value]) => {
-        delete value[1]?.lineHeight
-        return [key, value]
+        delete value[1]?.lineHeight;
+        return [key, value];
       })),
       // Merge line height with defaults
       lineHeight: Object.assign(config.theme.lineHeight, {
@@ -116,9 +137,9 @@ export default plugin.withOptions((
       }),
     },
     corePlugins: {
-      // We override the default lineHeight plugin.
+      // We override the default lineHeight & font size utilities.
       lineHeight: false,
+      fontSize: false,
     },
-  }
-})
-
+  };
+});
