ac8035feca7337611f27b3633bbb5aac49cbc6b9
Martijn Cuppens <martijn.cuppens@gmail.com>
2024-08-20 11:17:37 +0200

    New line heights

--- src/override-config.js
+++ src/override-config.js
@@ -1,5 +1,9 @@
 import {theme} from 'tailwindcss/stubs/config.full';
 
+const calculatedLineHeight = (lineHeight) => {
+  return `max(1em, ${lineHeight} * min(4em / 9 + 2rem / 3, min((2em + 1rem) / 3, (4em - 1rem) / 3)))`;
+}
+
 export default {
   theme: {
     // Remove line height from font sizes
@@ -9,11 +13,11 @@ export default {
     })),
     // Merge line height with defaults
     lineHeight: Object.assign(theme.lineHeight, {
-      tight: 'min(1.25em, 1em + .25rem)',
-      snug: 'min(1.375em, 1em + .375rem)',
-      normal: 'min(1.5em, 1em + .5rem)',
-      relaxed: 'min(1.625em, 1em + .625rem)',
-      loose: 'min(2em, 1em + 1rem)',
+      tight: calculatedLineHeight(1.25),
+      snug: calculatedLineHeight(1.375),
+      normal: calculatedLineHeight(1.5),
+      relaxed: calculatedLineHeight(1.625),
+      loose: calculatedLineHeight(2),
     }),
   },
   corePlugins: {
