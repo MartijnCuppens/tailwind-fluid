162d5735c23a87bce2f18c41bff2c46a09134e06
Martijn Cuppens <martijn.cuppens@gmail.com>
2024-08-20 20:58:58 +0200

    Remove redundant fallback

--- src/set-default-line-height.js
+++ src/set-default-line-height.js
@@ -2,7 +2,7 @@ export default ({addBase, theme}) => {
   // Define the base line-height in the root.
   addBase({
     ':root': {
-      '--tw-lh': theme('lineHeight')?.normal ?? 'min(1.5em, 1em + .5rem)', // Fail-safe when normal is line height is removed from config
+      '--tw-lh': theme('lineHeight')?.normal,
     },
     // Add line height for elements that have a font size set by default.
     'small,rt': {
